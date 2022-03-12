8040d2c1da1a8eabba0ba608536608607232fe24
XothiA <huaxia889900@126.com>
2022-03-12 15:34:21 +0800

    add some settings.

---
+++ src/test/java/TestFunc.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : xothia
+ * @version : 1.0
+ * @Project : ModbusTransMQTT
+ * @Package : java
+ * @ClassName : TestFunc.java
+ * @createTime : 2022/3/12 15:38
+ * @Email : huaxia889900@126.com
+ * @Description :测试类
+ */
+public class TestFunc {
+}
