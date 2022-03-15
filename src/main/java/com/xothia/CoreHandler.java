4c09f8c319527d6c871e5d7d1e7af0b83b93431b
XothiA <huaxia889900@126.com>
2022-03-15 15:42:35 +0800

    add paho and make some progress.

--- src/main/java/com/xothia/CoreHandler.java
+++ src/main/java/com/xothia/CoreHandler.java
@@ -2,7 +2,11 @@ package com.xothia;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.DefaultEventLoopGroup;
+import io.netty.handler.codec.mqtt.MqttConnAckMessage;
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
 import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.handler.codec.mqtt.MqttMessageBuilders;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,10 +21,22 @@ import io.netty.handler.codec.mqtt.MqttMessage;
  * @Description :
  */
 public class CoreHandler extends ChannelInboundHandlerAdapter {
+    private final static DefaultEventLoopGroup group = new DefaultEventLoopGroup();
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if(msg instanceof MqttMessage){
+        if(msg instanceof MqttMessage){ //接受到mqtt消息时
             System.out.println("mqtt msg inbound.");
+            MqttConnAckMessage ackMessage = MqttMessageBuilders.connAck().returnCode(MqttConnectReturnCode.CONNECTION_ACCEPTED).build();
+            ctx.writeAndFlush(ackMessage);
+            System.out.println("write complete.");
+            group.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("this is a task.");
+                }
+            });
+
         }
         else {
             super.channelRead(ctx, msg); //向后传递msg
