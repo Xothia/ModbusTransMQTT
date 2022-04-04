a3d858e0cc989ebb9ebfda0a7fb8ae3febc5303a
XothiA <huaxia889900@126.com>
2022-04-05 00:28:16 +0800

    change mqtt client impl.

--- src/test/java/TestFunc.java
+++ src/test/java/TestFunc.java
@@ -1,14 +1,14 @@
-import com.serotonin.modbus4j.ModbusFactory;
-import com.serotonin.modbus4j.ModbusMaster;
-import com.serotonin.modbus4j.ip.IpParameters;
-import com.serotonin.modbus4j.msg.WriteRegisterRequest;
-import com.serotonin.modbus4j.msg.WriteRegisterResponse;
 import com.xothia.MqttProxy;
 import de.gandev.modjn.example.Example;
 import io.netty.buffer.ByteBuf;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
-import nl.jk5.mqtt.*;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
+import io.netty.handler.codec.mqtt.MqttVersion;
+import org.jetlinks.mqtt.client.MqttClient;
+import org.jetlinks.mqtt.client.MqttClientConfig;
+import org.jetlinks.mqtt.client.MqttConnectResult;
+import org.jetlinks.mqtt.client.MqttHandler;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -50,23 +50,23 @@ public class TestFunc {
 
     @Test
     public void test4(){
-        IpParameters ip = new IpParameters();
-        ip.setHost("192.168.31.32");
-        ModbusMaster tcpMaster = new ModbusFactory().createTcpMaster(ip, true);
-        try{
-            tcpMaster.init();
-            //ReadHoldingRegistersResponse modbusResponse = (ReadHoldingRegistersResponse)tcpMaster.send(new ReadHoldingRegistersRequest(1, 0, 2));
-            WriteRegisterResponse modbusResponse = (WriteRegisterResponse)tcpMaster.send(new WriteRegisterRequest(2, 1, 53));
-            if(modbusResponse.isException()){
-                System.out.println(modbusResponse.getExceptionMessage());
-            }
-            else{
-                //System.out.println(Arrays.toString(modbusResponse.getShortData()));
-                System.out.println(modbusResponse.getWriteValue());
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+//        IpParameters ip = new IpParameters();
+//        ip.setHost("192.168.31.32");
+//        ModbusMaster tcpMaster = new ModbusFactory().createTcpMaster(ip, true);
+//        try{
+//            tcpMaster.init();
+//            //ReadHoldingRegistersResponse modbusResponse = (ReadHoldingRegistersResponse)tcpMaster.send(new ReadHoldingRegistersRequest(1, 0, 2));
+//            WriteRegisterResponse modbusResponse = (WriteRegisterResponse)tcpMaster.send(new WriteRegisterRequest(2, 1, 53));
+//            if(modbusResponse.isException()){
+//                System.out.println(modbusResponse.getExceptionMessage());
+//            }
+//            else{
+//                //System.out.println(Arrays.toString(modbusResponse.getShortData()));
+//                System.out.println(modbusResponse.getWriteValue());
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
 
     }
     @Test
@@ -75,47 +75,30 @@ public class TestFunc {
     }
 
     @Test
-    public void test6()throws Exception{
-        //Example.main(null);
-        MqttClientConfig config = new MqttClientConfig();
-        config.setClientId("gwdxm71ywNi.N5Ly9CzuLWIyANQmQ5Jl|securemode=2,signmethod=hmacsha256,timestamp=1649002653400|");
-        config.setUsername("N5Ly9CzuLWIyANQmQ5Jl&gwdxm71ywNi");
-        config.setPassword("d6e3cc2528127203ccf832f7dfe2c19ff57f83a0d61519805d8d362f6949d731");
-        MqttClientImpl mqttClient = new MqttClientImpl(config);
-        Future<MqttConnectResult> connectResult = mqttClient.connect("iot-06z00d3qre8b6ub.mqtt.iothub.aliyuncs.com",1883);
-        connectResult.sync();
-
-//        connectResult.addListener(new GenericFutureListener<Future<? super MqttConnectResult>>() {
-//            @Override
-//            public void operationComplete(Future<? super MqttConnectResult> future) throws Exception {
-//                if(future.isSuccess()){
-//                    System.out.println("connect success.");
-//                }
-//                else{
-//                    System.out.println("connect failed.");
-//                }
-//            }
-//        });
-        //connectResult.await();
-        mqttClient.on("/broadcast/gwdxm71ywNi/hahah", new MqttHandler() {
+    public void test6() throws Exception{
+        EventLoopGroup loop = new NioEventLoopGroup();
+        //MqttClient mqttClient = new MqttClientImpl(); mqttClient.creat
+        MqttClient mqttClient = MqttClient.create(new MqttClientConfig(), new MqttHandler() {
             @Override
             public void onMessage(String s, ByteBuf byteBuf) {
-                System.out.println("arrived.");
-                System.out.println(s);
+
             }
         });
 
-        while(true){
-            if(connectResult.isSuccess()){
-                System.out.println("success");
-            }else{
-                System.out.println("failed");
-            }
-            Thread.sleep(1000);
+        mqttClient.setEventLoop(loop);
+        mqttClient.getClientConfig().setClientId("gwdxm71ywNi.N5Ly9CzuLWIyANQmQ5Jl|securemode=2,signmethod=hmacsha256,timestamp=1649002653400|");
+        mqttClient.getClientConfig().setUsername("N5Ly9CzuLWIyANQmQ5Jl&gwdxm71ywNi");
+        mqttClient.getClientConfig().setPassword("d6e3cc2528127203ccf832f7dfe2c19ff57f83a0d61519805d8d362f6949d731");
+
+        mqttClient.getClientConfig().setProtocolVersion(MqttVersion.MQTT_3_1_1);
+
+        MqttConnectResult result = mqttClient.connect("iot-06z00d3qre8b6ub.mqtt.iothub.aliyuncs.com", 1883).await().get();
+        if (result.getReturnCode() != MqttConnectReturnCode.CONNECTION_ACCEPTED) {
+            System.out.println("error:" + result.getReturnCode());
+        } else {
+            System.out.println("success");
+//    mqttClient.publish("test", Unpooled.copiedBuffer("{\"type\":\"event\"}", StandardCharsets.UTF_8));
         }
-
-
-
 
     }
 
