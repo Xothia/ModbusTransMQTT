c2baee7091f704b79d25d0b258c8710c02d06672
XothiA <huaxia889900@126.com>
2022-04-08 00:36:55 +0800

    add Modbus Slave Interface.

---
+++ src/main/java/com/xothia/bean/modbusSlave/MbSlave.java
@@ -0,0 +1,21 @@
+package com.xothia.bean.modbusSlave;
+
+import com.xothia.bean.mqttClient.MqttClientManager;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : xothia
+ * @version : 1.0
+ * @Project : ModbusTransMQTT
+ * @Package : com.xothia.bean.modbusSlave
+ * @ClassName : .java
+ * @createTime : 2022/4/7 15:45
+ * @Email : huaxia889900@126.com
+ * @Description : Modbus Slave Interface
+ */
+public interface MbSlave {
+    MqttClientManager getMqttClientManager();
+
+    MbSlaveUpstreamPatten[] getUpstreamPatten();
+}
